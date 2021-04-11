5e41d8c5e93080aa0328ef8c6d996d1884c6b2fe
msuzun <sevki_uzun53@hotmail.com>
2021-04-11 21:07:42 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,9 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+
+#question 2
+n=int(input("Bir sayi giriniz."))
+y=1
+for i in range(0,n):
+    if i%2==0:
+        print("%d'inci cift sayi = %d"%(,i))
+        y+=1

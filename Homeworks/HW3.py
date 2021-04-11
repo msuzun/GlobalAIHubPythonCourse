238aebbb8a79d50cf0954b98ecf6262c81b1c34e
msuzun <sevki_uzun53@hotmail.com>
2021-04-11 21:08:18 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,20 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+
+#question 3
+
+loginUsernamePassword={"admin":"admin",
+                       "root":"1234567",
+                       "login":"qwerty"}
+
+print(loginUsernamePassword.keys())
+
+username=input("Kullanıcı adinizi giriniz")
+password=input("Parolanızı giriniz")
+
+if username in loginUsernamePassword.keys():
+    if loginUsernamePassword[username] == password:
+        print("Parolaniz dogru")
+    else:
+        print("Kullanici adiniz veya parolaniz yanlis")
+else:
+    print("Kullanici adiniz veya parolaniz yanlis")
